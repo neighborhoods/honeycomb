432c28f8d266f8ea0707f6ea7b18970e69fad28c
George Wood <george.wood@55places.com>
2020-06-16 11:48:01 -0500

    Re-add mistakenly removed line from init

--- honeycomb/__init__.py
+++ honeycomb/__init__.py
@@ -3,9 +3,11 @@ from .create_table import create_table_from_df
 from .describe_table import describe_table
 from .meta import get_table_storage_type_from_metadata
 from .querying import run_query
+from . import check
 
 __all__ = [
     'append_table',
+    'check',
     'create_table_from_df',
     'describe_table',
     'get_table_storage_type_from_metadata',
