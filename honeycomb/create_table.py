f1a391252d474ae592acc5679e01b0423ddfdef5
George Wood <george.wood@55places.com>
2020-06-23 16:30:17 -0500

    DE-111 | Usage Documentation (#10)
    * DE-111 | Usage Documentation
    
    * Added clarification on BigQuery SQL differences

--- honeycomb/create_table.py
+++ honeycomb/create_table.py
@@ -110,7 +110,7 @@ def check_for_comments(table_comment, columns, col_comments):
                          'Columns that require comments: ' +
                          ', '.join(cols_wo_comment))
 
-# NOTE add presto support?
+
 def create_table_from_df(df, table_name, schema='experimental',
                          dtypes=None, path=None, filename=None,
                          table_comment=None, col_comments=None,
