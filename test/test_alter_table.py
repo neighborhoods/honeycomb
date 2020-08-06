635ce91e10c18e564f87ca07c86ba85bfbacb24c
George Wood <george.wood@55places.com>
2020-08-06 11:31:02 -0500

    DE-142 | Timezone Conversions (#18)
    * DE-142 | Timezone Conversions
    
    * Automatic timezone conversion - needs tests
    
    * Added tests
    
    * changelog
    
    * Add 'dtypes' parameter to append fn
    
    * Clarified docstrings
    
    * one more merge conflict
    
    * Import cleanup

--- test/test_alter_table.py
+++ test/test_alter_table.py
@@ -1,12 +1,15 @@
-from honeycomb.alter_table import add_partitions
+from honeycomb.alter_table import add_partition
 
 
-def test_add_partitions_builds_path(mocker):
+def test_add_partition_builds_path(mocker):
     mocker.patch('honeycomb.run_query.lake_query', return_value=False)
-    partitions = [('year_partition', '2020'), ('month_partition', '01')]
-    expected_path = '/'.join([partition[1] for partition in partitions]) + '/'
+    mocker.patch('honeycomb.check.partition_existence', return_value=False)
 
-    actual_path = add_partitions(table_name='table', schema='experimental',
-                                 partitions=partitions)
+    partition_values = {'year_partition': '2020', 'month_partition': '01'}
+
+    expected_path = '/'.join(partition_values.values()) + '/'
+
+    actual_path = add_partition(table_name='table', schema='experimental',
+                                partition_values=partition_values)
 
     assert actual_path == expected_path
