7cbc1803d9dfe4e57bd2346ffabb02a94e4dc7f6
George Wood <george.wood@55places.com>
2020-10-20 13:19:32 -0500

    Envvar Reading for Lake Address

--- honeycomb/hive.py
+++ honeycomb/hive.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 
 from honeycomb.connection import get_db_connection
@@ -14,12 +16,14 @@ def run_lake_query(query, engine='presto'):
             Use 'presto' for faster, ad-hoc/experimental querie
             Use 'hive' for slower but more robust queries
     """
+    addr = os.getenv('HC_LAKE_ADDRESS', 'localhost')
+
     query_fns = {
         'presto': _presto_query,
         'hive': _hive_query,
     }
     query_fn = query_fns[engine]
-    df = query_fn(query)
+    df = query_fn(query, addr)
     return df
 
 
@@ -34,23 +38,23 @@ def _query_returns_df(query):
     return False
 
 
-def _hive_query(query):
+def _hive_query(query, addr):
     """
     Hive-specific query function
     Note: uses an actual connection, rather than a connection cursor
     """
     if _query_returns_df(query):
-        with get_db_connection('hive', cursor=False) as conn:
+        with get_db_connection('hive', addr=addr, cursor=False) as conn:
             df = pd.read_sql(query, conn)
             # Cleans table prefixes from column names, which are added by Hive
             df.columns = df.columns.str.replace(r'^.*\.', '')
             return df
     else:
-        with get_db_connection('hive', cursor=True) as conn:
+        with get_db_connection('hive', addr=addr, cursor=True) as conn:
             conn.execute(query)
 
 
-def _presto_query(query):
+def _presto_query(query, addr):
     """
     Presto-specific query function
     Note: uses an actual connection, rather than a connection cursor
@@ -58,9 +62,9 @@ def _presto_query(query):
     # Presto does not have a notion of a persistent connection, so closing
     # is unnecessary
     if _query_returns_df(query):
-        conn = get_db_connection('presto', cursor=False)
+        conn = get_db_connection('presto', addr=addr, cursor=False)
         df = pd.read_sql(query, conn)
         return df
     else:
-        conn = get_db_connection('presto', cursor=True)
+        conn = get_db_connection('presto', addr=addr, cursor=True)
         conn.execute(query)
